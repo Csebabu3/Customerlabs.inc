a5e325fc43770eda212178ce41d1888e604f17fc
babu <csebabu3@gmail.com>
2024-03-21 16:32:32 +0530

    frist commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddSegment from './Component/AddSegment';
+
+function App() {
+  return (
+    <div className="App">
+        <AddSegment />
+    </div>
+  );
+}
+
+export default App;
